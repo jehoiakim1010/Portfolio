1bd625faa86cd8583c83e906ac6ea441fa50f7c5
jehoiakim1010 <jehoiakimtaniegra@gmail.com>
2024-06-08 21:22:34 +0800

    modified

--- js/main.js
+++ js/main.js
@@ -1,121 +1,154 @@
 jQuery(document).ready(function ($) {
 
-    'use strict';
+  // navigation click actions 
+  $('.scroll-link').on('click', function (event) {
+    event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    scrollToID('#' + sectionID, 750);
+  });
+  // scroll to top action
+  $('.scroll-top').on('click', function (event) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+  });
+  // mobile nav toggle
+  $('#nav-toggle').on('click', function (event) {
+    event.preventDefault();
+    $('#main-nav').toggleClass("open");
+  });
 
-    $(window).load(function () { // makes sure the whole site is loaded
-        $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-        $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    })
+  // scroll function
+  function scrollToID(id, speed) {
+    var offSet = 0;
+    var targetOffset = $(id).offset().top - offSet;
+    var mainNav = $('#main-nav');
+    $('html,body').animate({ scrollTop: targetOffset }, speed);
+    if (mainNav.hasClass("open")) {
+      mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+      mainNav.removeClass("open");
+    }
+  }
+  if (typeof console === "undefined") {
+    console = {
+      log: function () { }
+    };
+  }
+  'use strict';
 
-
-    $(function () {
-
-        function showSlide(n) {
-            // n is relative position from current slide
-
-            // unbind event listener to prevent retriggering
-            $body.unbind("mousewheel");
-
-            // increment slide number by n and keep within boundaries
-            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length - 1);
-
-            var displacment = window.innerWidth * currSlide;
-            // translate slides div across to appropriate slide
-            $slides.css('transform', 'translateX(-' + displacment + 'px)');
-            // delay before rebinding event to prevent retriggering
-            setTimeout(bind, 700);
-
-            // change active class on link
-            $('nav a.active').removeClass('active');
-            $($('a')[currSlide]).addClass('active');
-
-        }
-
-        function bind() {
-            $body.bind('false', mouseEvent);
-        }
-
-        function mouseEvent(e, delta) {
-            // On down scroll, show next slide otherwise show prev slide
-            showSlide(delta >= 0 ? -1 : 1);
-            e.preventDefault();
-        }
-
-        $('nav a, .main-btn a').click(function (e) {
-            // When link clicked, find slide it points to
-            var newslide = parseInt($(this).attr('href')[1]);
-            // find how far it is from current slide
-            var diff = newslide - currSlide - 1;
-            showSlide(diff); // show that slide
-            e.preventDefault();
-        });
-
-        $(window).resize(function () {
-            // Keep current slide to left of window on resize
-            var displacment = window.innerWidth * currSlide;
-            $slides.css('transform', 'translateX(-' + displacment + 'px)');
-        });
-
-        // cache
-        var $body = $('body');
-        var currSlide = 0;
-        var $slides = $('.slides');
-        var $slide = $('.slide');
-
-        // give active class to first link
-        $($('nav a')[0]).addClass('active');
-
-        // add event listener for mousescroll
-        $body.bind('false', mouseEvent);
-    })
+  $(window).load(function () { // makes sure the whole site is loaded
+    $(".seq-preloader").fadeOut(); // will first fade out the loading animation
+    $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+  })
 
 
-    $('#form-submit .date').datepicker({
+  $(function () {
+
+    function showSlide(n) {
+      // n is relative position from current slide
+
+      // unbind event listener to prevent retriggering
+      $body.unbind("mousewheel");
+
+      // increment slide number by n and keep within boundaries
+      currSlide = Math.min(Math.max(0, currSlide + n), $slide.length - 1);
+
+      var displacment = window.innerWidth * currSlide;
+      // translate slides div across to appropriate slide
+      $slides.css('transform', 'translateX(-' + displacment + 'px)');
+      // delay before rebinding event to prevent retriggering
+      setTimeout(bind, 700);
+
+      // change active class on link
+      $('nav a.active').removeClass('active');
+      $($('a')[currSlide]).addClass('active');
+
+    }
+
+    function bind() {
+      $body.bind('false', mouseEvent);
+    }
+
+    function mouseEvent(e, delta) {
+      // On down scroll, show next slide otherwise show prev slide
+      showSlide(delta >= 0 ? -1 : 1);
+      e.preventDefault();
+    }
+
+    $('nav a, .main-btn a').click(function (e) {
+      // When link clicked, find slide it points to
+      var newslide = parseInt($(this).attr('href')[1]);
+      // find how far it is from current slide
+      var diff = newslide - currSlide - 1;
+      showSlide(diff); // show that slide
+      e.preventDefault();
     });
 
-
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 100) {
-            $(".header").addClass("active");
-        } else {
-            //remove the background property so it comes transparent again (defined in your css)
-            $(".header").removeClass("active");
-        }
+    $(window).resize(function () {
+      // Keep current slide to left of window on resize
+      var displacment = window.innerWidth * currSlide;
+      $slides.css('transform', 'translateX(-' + displacment + 'px)');
     });
+
+    // cache
+    var $body = $('body');
+    var currSlide = 0;
+    var $slides = $('.slides');
+    var $slide = $('.slide');
+
+    // give active class to first link
+    $($('nav a')[0]).addClass('active');
+
+    // add event listener for mousescroll
+    $body.bind('false', mouseEvent);
+  })
+
+
+  $('#form-submit .date').datepicker({
+  });
+
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+      $(".header").addClass("active");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header").removeClass("active");
+    }
+  });
 
 
 });
 
 $(function () {
-    $(document).delegate(".chat-btn", "click", function () {
-        var value = $(this).attr("chat-value");
-        var name = $(this).html();
-        $("#chat-input").attr("disabled", false);
-        generate_message(name, 'self');
-    })
+  $(document).delegate(".chat-btn", "click", function () {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
 
-    $("#chat-circle").click(function () {
-        $("#chat-circle").toggle('scale');
-        $(".chat-box").toggle('scale');
-    })
+  $("#chat-circle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
 
-    $(".chat-box-toggle").click(function () {
-        $("#chat-circle").toggle('scale');
-        $(".chat-box").toggle('scale');
-    })
+  $(".chat-box-toggle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
 
 })
 
 var words = ['A Bit About Me', 'Im a Solo Indie Game Developer', '3D Artist', 'Web Developer', 'Software Developer', 'Computer Technician'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 40;
-    
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 40;
+
 var wordflick = function () {
   setInterval(function () {
     if (forwards) {
@@ -147,7 +180,7 @@ var wordflick = function () {
       }
     }
     $('.word').text(part);
-  },speed);
+  }, speed);
 };
 
 $(document).ready(function () {
